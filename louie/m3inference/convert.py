06fe2fc68fc8a5a1ceefa4fad79e9af403135480
Louie Sinadjan <louie_sinz@yahoo.co.uk>
2025-03-25 19:02:41 +0000

    m3inference hpc

--- louie/m3inference/convert.py
+++ louie/m3inference/convert.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import ast
 import re
+import glob
 
 def value_or_null(val):
     return None if pd.isna(val) else val
@@ -33,15 +34,22 @@ def extract_raw_description(val):
     except Exception as e:
         return None
     
+def load_dataset(main_dir, dir):
+    files = glob.glob(f'{main_dir}/{dir}/*.csv.gz')
+    # df_list = [pd.read_csv(f, compression = 'gzip') for f in files]
+    df_list = [pd.read_csv(f, compression = 'infer') for f in files]
+    df = pd.concat(df_list, ignore_index=True)
+    return df
+    
 
-csv_file = 'small_chunk.csv'
+# csv_file = '/user/work/bn22907/usc-x-24-us-election'
 # csv_file = 'aug_chunk_41.csv'
 
-jsonl_file = 'small_chunk.jsonl'
+# jsonl_file = 'small_chunk.jsonl'
 # jsonl_file = 'aug_chunk_41.jsonl'
 
-
-df = pd.read_csv(csv_file)
+jsonl_file = 'part_1.jsonl'
+df =load_dataset('/user/work/bn22907/usc-x-24-us-election/', 'part_1')
 
 with open(jsonl_file, 'w') as f:
     for _, row in df.iterrows():
