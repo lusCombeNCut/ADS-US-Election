c5e4fdeac473cbd3b216c676412775f9438b196e
atb85 <alex.bell.85@outlook.com>
2025-04-11 12:09:51 +0100

    fix: sentiment irony in topics

--- merge_timestamp.py
+++ merge_timestamp.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from glob import glob
 
-TOPICS_PATH = "./orlando_BERTopic_results_v3.csv"
+TOPICS_PATH = "./v3_results.csv"
 CHUNKS_PATH = "./sentiment_results"
 
 chunks = glob(f"{CHUNKS_PATH}/*.csv")
-topics = pd.read_csv(TOPICS_PATH, dtype={'id': str})
+topics = pd.read_csv(TOPICS_PATH, dtype={'id': str})[['id', 'topic']]
 chunks_df = [pd.read_csv(f, dtype={'id': str}) for f in chunks]
 print("ALL CHUNKS READ")
 dset = pd.concat(chunks_df, ignore_index=True)
 print("ALL CHUNKS CONCAT")
-merged = pd.merge(topics, dset, on="id", how='left')
-merged.to_csv("./BERTTopic_sentiment_irony.csv")
+merged = pd.merge(topics, dset, on="id", how='inner')
+merged.to_csv("./BERTTopic_sentiment_irony.csv", index=False)
